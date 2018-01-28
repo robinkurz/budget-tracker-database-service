2f47578c6ac827cdb730b5281099e60c744faaf6
kurz <robin.kurz@studentl.reutlingen-university.de>
2018-01-28 15:00:01 +0100

    postgres connection set up, was tested with the docker container of postgres

--- src/main/java/expense/Expense.java
+++ src/main/java/expense/Expense.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Expense {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private int amount;
