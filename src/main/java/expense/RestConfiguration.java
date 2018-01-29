730d8522de00e4e246714b1a306f6cf7f76b1480
Kurz <robin.kurz@capgemini.com>
2018-01-29 10:23:52 +0100

    removed indentation

--- src/main/java/expense/RestConfiguration.java
+++ src/main/java/expense/RestConfiguration.java
@@ -7,16 +7,16 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-    @Configuration
-    public class RestConfiguration {
+@Configuration
+public class RestConfiguration {
 
-        @Bean
-        public FilterRegistrationBean corsFilter() {
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
-            source.registerCorsConfiguration("/**", config);
-            FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-            bean.setOrder(0);
-            return bean;
-        }
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
+}
