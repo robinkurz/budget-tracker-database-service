1d9d3ca03635d0a605fa503b35e996fe518f68f4
Robin Kurz <robin.kurz@protonmail.com>
2018-03-11 20:03:37 +0100

    added gitignore

---
+++ src/main/java/expense/MonthlyExpense.java
@@ -0,0 +1,42 @@
+package expense;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private int amount;
+    private String reason;
+    private String date;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
