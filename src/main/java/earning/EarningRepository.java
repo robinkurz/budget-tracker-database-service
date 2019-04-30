6b60c8ec54b02bf12ff46d4c46e102e0f6f6134f
Robin Kurz <robin.kurz@protonmail.com>
2019-04-30 13:07:16 +0200

    Allow CrossOrigin calls

--- src/main/java/earning/EarningRepository.java
+++ src/main/java/earning/EarningRepository.java
@@ -2,7 +2,9 @@ package earning;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource( collectionResourceRel = "earnings", path="earnings")
 public interface EarningRepository extends PagingAndSortingRepository<Earning, Long> {
 }
--- src/main/java/expense/ExpenseRepository.java
+++ src/main/java/expense/ExpenseRepository.java
@@ -3,9 +3,11 @@ package expense;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 @RepositoryRestResource( collectionResourceRel = "expenses", path="expenses")
 public interface ExpenseRepository extends PagingAndSortingRepository<Expense, Long>{
 
